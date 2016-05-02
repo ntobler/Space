049f05375003f0f68488006540941f512ca18ba3
Nicolas <Nicolas@Nicolas-PC.home>
2016-05-02 22:17:09 +0200

    Design change

--- Space/src/com/ntobler/space/DeltaVGauge.java
+++ Space/src/com/ntobler/space/DeltaVGauge.java
@@ -3,6 +3,7 @@ package com.ntobler.space;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
 public class DeltaVGauge {
 
@@ -22,9 +23,9 @@ public class DeltaVGauge {
 			double rotationVelocity = lockOn.getRotationSpeed() * distance;
 			
 			
-			progradeVelocity = Orbit.getProgradeVelocity(ship, lockOn) - rotationVelocity;
+			progradeVelocity = Orbit.getProgradeVelocity(ship, lockOn);// - rotationVelocity;
 			//progradeVelocity = Orbit.getProgradeVelocity(ship, lockOn);
-			radialVelocity = Orbit.getRadialVelocity(ship, lockOn);
+			radialVelocity = -Orbit.getRadialVelocity(ship, lockOn);
 
 			lockOnPresent = true;
 		}
@@ -44,27 +45,79 @@ public class DeltaVGauge {
 		
 		double pos;
 		
-		g2.setColor(Color.RED);
 		
-		g2.draw(new Line2D.Double(-length, 0, length, 0));
-		g2.draw(new Line2D.Double(0, -length, 0, length));
 		
-		if (lockOnPresent) {
+		//g2.draw(new Line2D.Double(-length, 0, length, 0));
+		//g2.draw(new Line2D.Double(0, -length, 0, length));
+		
+		/*if (lockOnPresent) {
 		
 			pos = progradeVelocity * scaleFactor; 
 			if (pos > length) pos = length;
 			if (pos < -length) pos = -length;
 			
+			g2.setColor(Color.RED);
 			g2.draw(new Line2D.Double(pos, -size, pos, size));
-			
-			
-			
+			g2.setColor(new Color(255,0,0,100));;
+			if (pos > 0) {
+				g2.fill(new Rectangle2D.Double(0, -size, pos, size*2));
+			}
+			else {
+				g2.fill(new Rectangle2D.Double(pos, -size, -pos, size*2));
+			}
 			
 			pos = radialVelocity * scaleFactor; 
 			if (pos > length) pos = length;
 			if (pos < -length) pos = -length;
-			
+			g2.setColor(Color.RED);
 			g2.draw(new Line2D.Double(-size, pos, size, pos));
+			g2.setColor(new Color(255,0,0,100));;
+			if (pos > 0) {
+				g2.fill(new Rectangle2D.Double(-size, 0, size*2, pos));
+			}
+			else {
+				g2.fill(new Rectangle2D.Double(-size, pos, size*2, -pos));
+			}
+		}*/
+		
+		if (lockOnPresent) {
+			
+			
+			double rad = radialVelocity * scaleFactor; 
+			if (rad > length) rad = length;
+			if (rad < -length) rad = -length;
+			
+			double pro = progradeVelocity * scaleFactor;
+			if (pro > length) pro = length;
+			if (pro < -length) pro = -length;
+			
+			g2.setColor(Color.RED);
+			g2.draw(new Line2D.Double(0, rad, pro, rad));
+			g2.draw(new Line2D.Double(pro, 0, pro, rad));
+			
+			double x, y, width, height;
+			
+			
+			if (rad > 0) {
+				y = 0;
+				height = rad;
+			}
+			else{
+				y = rad;
+				height = -rad;
+			}
+			
+			if (pro > 0) {
+				x = 0;
+				width = pro;
+			}
+			else{
+				x = pro;
+				width = -pro;
+			}
+			
+			g2.setColor(new Color(255,0,0,100));
+			g2.fill(new Rectangle2D.Double(x, y, width, height));
 		}
 		
 	}
